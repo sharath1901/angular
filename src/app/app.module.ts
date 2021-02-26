9c80d731c68b5d0b32227dbae2448d771f37de0d
Sajid Ali Khan <sajid.hmdrd@gmail.com>
2021-02-27 01:16:54 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NewsComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserM
